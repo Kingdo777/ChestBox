d3d1c387caa61fbdbbb1e81c4cc7c27a0cfc8254
kingdo <1440852110@qq.com>
2023-12-21 12:09:04 +0800

    Share the ipc namespace of state-function-1, not the host's ipc namespace.

--- clean-shm.sh
+++ clean-shm.sh
@@ -1,1 +1,4 @@
-ipcs -m | grep '0x7777' | awk '{print $2}' | xargs -n 1 sudo ipcrm -m
+#!/bin/bash
+
+#ipcs -m | grep '0x7777' | awk '{print $2}' | xargs -n 1 sudo ipcrm -m
+docker exec -it wsk0_kingdo_guest_StateFunction1 sh -c "ipcs -m | grep '0x7777' | awk '{print \$2}' | xargs -n 1 ipcrm -m"
