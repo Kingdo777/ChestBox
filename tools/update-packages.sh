615cfed6d6d85a2df94831b1bf5a330928fbc77b
kingdo <1440852110@qq.com>
2023-12-24 17:55:15 +0800

    Strengthen update-packages.sh

--- tools/update-packages.sh
+++ tools/update-packages.sh
@@ -2,15 +2,24 @@
 
 ProjectRoot=$(dirname "$(dirname "$(realpath "$0")")")
 
+pushd "$ProjectRoot/StateFunction/core" || exit
+  cmake --build cmake-build-debug --target python-extend-statefunction
+  cmake --build cmake-build-debug --target python-extend-ipc
+popd || exit
+
 pushd "$ProjectRoot/StateFunction/core/src/state-function-action/ipc-py-extend" || exit
+  rm -rf build dist ipc.egg-info
   python3.10 setup.py build bdist_wheel
   cp dist/ipc-1.0.0-cp310-cp310-linux_x86_64.whl "$ProjectRoot/OpenWhisk/runtime-python/core/python310Action/"
+  cp dist/ipc-1.0.0-cp310-cp310-linux_x86_64.whl "$ProjectRoot/Evaluation/0.SimpleTest/"
   "$ProjectRoot"/venv/bin/pip install --upgrade .
 popd || exit
 
 pushd "$ProjectRoot/StateFunction/core/src/state-function-library/statefunction-py-extend" || exit
+  rm -rf build dist statefunction.egg-info
   python3.10 setup.py build bdist_wheel
   cp dist/statefunction-3.0.0-cp310-cp310-linux_x86_64.whl "$ProjectRoot/OpenWhisk/runtime-python/core/python310Action/"
+  cp dist/statefunction-3.0.0-cp310-cp310-linux_x86_64.whl "$ProjectRoot/Evaluation/0.SimpleTest/"
   "$ProjectRoot"/venv/bin/pip install --upgrade .
 popd || exit
 
