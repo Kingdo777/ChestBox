e8a2658b2edcc82e9d030b3c8f1def5c7b677e87
kingdo <1440852110@qq.com>
2023-12-25 18:33:40 +0800

    Initialize Spdlog to support closing it or setting the log level

--- tools/clean-shm.sh
+++ tools/clean-shm.sh
@@ -2,3 +2,4 @@
 
 #ipcs -m | grep '0x7777' | awk '{print $2}' | xargs -n 1 sudo ipcrm -m
 docker exec -it wsk0_kingdo_guest_StateFunction1 sh -c "ipcs -m | grep '0x7777' | awk '{print \$2}' | xargs -n 1 ipcrm -m"
+docker exec -it wsk0_kingdo_guest_StateFunction2 sh -c "ipcs -q | grep '0x00001' | awk '{print \$2}' | xargs -n 1 ipcrm -q"
